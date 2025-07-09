44f48ba18443963e2cf77cf90b377c44d5d7d51c
chillin9panda <duressamarkos9@gmail.com>
2025-07-09 21:48:50 +0300

    allow scan for pkgs that branch off from root pkg

--- src/main/java/chillin9panda/booknest/BookNestApplication.java
+++ src/main/java/chillin9panda/booknest/BookNestApplication.java
@@ -2,12 +2,12 @@ package chillin9panda.booknest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "chillin9panda")
 public class BookNestApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BookNestApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(BookNestApplication.class, args);
+  }
 }

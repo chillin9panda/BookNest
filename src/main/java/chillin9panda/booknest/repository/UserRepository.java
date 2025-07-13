09cf303d005d6fb51e7f90aa2605d40480b72e4c
chillin9panda <duressamarkos9@gmail.com>
2025-07-13 10:18:54 +0300

    Login configured

--- src/main/java/chillin9panda/booknest/repository/UserRepository.java
+++ src/main/java/chillin9panda/booknest/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package chillin9panda.booknest.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import chillin9panda.booknest.model.User;
 
 public interface UserRepository extends JpaRepository<User, String> {
   boolean existsByUsername(String username);
+
+  Optional<User> findByUsername(String username);
 
 }
